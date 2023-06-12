23b3751c9432f4273db3fe80e9505d22064484b9
doranmartin <doranmartin1994@gmail.com>
2023-06-12 10:01:07 -0400

    Created random dir

---
+++ random/random.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  vector<string> msg {"Hi", "Hannah", "I", "love", "you"};
+
+  cout << msg[1];
+
+  cout << endl;
+
+}
